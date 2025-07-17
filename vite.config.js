b66bba4e06832766b28bf3145e16cb6932b56ddb
manasranjan <manasaarukha99@gmail.com>
2025-07-17 11:55:50 +0530

    my changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Weatherly/", // 👈 important!
+  base: "/", // 👈 important!
   plugins: [react()],
 });
