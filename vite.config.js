20b80ac3ae2b47b2de39f2b7c20d1bacf296577f
MANAS RANJAN ARUKHA <140185268+manasranjanarukha@users.noreply.github.com>
2025-08-07 12:45:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // 👈 important!
+  base: "'/Weatherly/'", // 👈 important!
   plugins: [react()],
 });
